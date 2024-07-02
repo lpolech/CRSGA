376d5de4bb521891f92a54e47ce551846e4799e2
Lukasz Olech <lukasz.piotr.olech@gmail.com>
2024-07-02 20:36:31 +1000

    cost toggle

--- src/algorithms/visualization/KmeansClusterisation.java
+++ src/algorithms/visualization/KmeansClusterisation.java
@@ -21,6 +21,22 @@ public class KmeansClusterisation<PROBLEM extends BaseProblemRepresentation> {
 
     private HashMap<String, Double> qualityMeasureValues;
 
+    public void setDisableCostEdgePromotion(boolean disableCostEdgePromotion) {
+        this.disableCostEdgePromotion = disableCostEdgePromotion;
+    }
+
+    public boolean isDisableCostEdgePromotion() {
+        return disableCostEdgePromotion;
+    }
+
+    public boolean isDisableTravelEdgePromotion() {
+        return disableTravelEdgePromotion;
+    }
+
+    public void setDisableTravelEdgePromotion(boolean disableTravelEdgePromotion) {
+        this.disableTravelEdgePromotion = disableTravelEdgePromotion;
+    }
+
     private boolean disableCostEdgePromotion;
 
     private boolean disableTravelEdgePromotion;
